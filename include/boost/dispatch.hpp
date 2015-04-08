f8a90d2d317c76091e43f5e6d331224ef2b9d224
Charly Chevalier <charly.chevalier@numscale.com>
2015-04-08 20:22:32 +0200

    Main dispacth.hpp headers

---
+++ include/boost/dispatch.hpp
@@ -0,0 +1,44 @@
+//==================================================================================================
+/*!
+  @file
+
+  Provides a kitchen sink include files for the Boost.Dispatch library
+
+  @copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_HPP_INCLUDED
+#define BOOST_DISPATCH_HPP_INCLUDED
+
+namespace boost { namespace dispatch
+{
+  namespace concept
+  {
+    /*!
+      @defgroup group-concept Concepts
+      Concepts defined by the library
+    **/
+  }
+
+  namespace meta
+  {
+    /*!
+      @defgroup group-meta Meta-programming Utilities
+      Meta-programming related components
+    **/
+  }
+
+  namespace detail
+  {
+    /*!
+      @defgroup group-details Implementation details
+      Implementation details related components
+    **/
+  }
+
+} }
+#endif
