c5e47e61305abd97a78896cb77ce3d54e7643b2c
Charly Chevalier <charly.chevalier@numscale.com>
2015-06-12 20:06:11 +0200

    Add factory_of metafunction

---
+++ test/meta/factory_of.cpp
@@ -0,0 +1,21 @@
+//==================================================================================================
+/*
+  Copyright 2012 - 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+*/
+//==================================================================================================
+#include <boost/dispatch/meta/factory_of.hpp>
+
+#include <nstest/unit.hpp>
+#include <nstest/unit/tests/types.hpp>
+
+NSTEST_CASE( "factory_of of basic types is meta-identity")
+{
+  NSTEST_TYPE_IS( boost::dispatch::meta::factory_of<float>::type<int>       , int );
+  NSTEST_TYPE_IS( boost::dispatch::meta::factory_of<float&>::type<int>      , int );
+  NSTEST_TYPE_IS( boost::dispatch::meta::factory_of<float&&>::type<int>     , int );
+  NSTEST_TYPE_IS( boost::dispatch::meta::factory_of<float const>::type<int> , int );
+  NSTEST_TYPE_IS( boost::dispatch::meta::factory_of<float const&>::type<int>, int );
+}
