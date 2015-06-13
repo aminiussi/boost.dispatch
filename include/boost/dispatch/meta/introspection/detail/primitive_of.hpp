45f3cc2f90c6f6a156ed03a3f22b0aa6aa225525
Charly Chevalier <charly.chevalier@numscale.com>
2015-06-13 21:12:17 +0200

    A bit more structure in meta/ folder

--- include/boost/dispatch/meta/introspection/detail/primitive_of.hpp
+++ include/boost/dispatch/meta/introspection/detail/primitive_of.hpp
@@ -11,10 +11,10 @@
 
 **/
 //==================================================================================================
-#ifndef BOOST_DISPATCH_META_DETAIL_PRIMITIVE_OF_HPP_INCLUDED
-#define BOOST_DISPATCH_META_DETAIL_PRIMITIVE_OF_HPP_INCLUDED
+#ifndef BOOST_DISPATCH_META_INTROSPECTION_DETAIL_PRIMITIVE_OF_HPP_INCLUDED
+#define BOOST_DISPATCH_META_INTROSPECTION_DETAIL_PRIMITIVE_OF_HPP_INCLUDED
 
-#include <boost/dispatch/meta/value_of.hpp>
+#include <boost/dispatch/meta/introspection/value_of.hpp>
 
 namespace boost { namespace dispatch { namespace detail
 {
