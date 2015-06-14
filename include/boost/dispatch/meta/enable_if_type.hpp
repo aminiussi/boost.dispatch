f4c44d7a2729febb6183135d5cc57802a92cf2f7
Charly Chevalier <charly.chevalier@numscale.com>
2015-06-14 20:35:44 +0200

    Added enable_if_type helper

---
+++ include/boost/dispatch/meta/enable_if_type.hpp
@@ -0,0 +1,31 @@
+//==================================================================================================
+/*!
+  @file
+
+  Defines the meta::enable_if_type SFINAE helper
+
+  @copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_META_ENABLE_IF_TYPE_HPP_INCLUDED
+#define BOOST_DISPATCH_META_ENABLE_IF_TYPE_HPP_INCLUDED
+
+namespace boost { namespace dispatch { namespace meta
+{
+  /*!
+    @brief SFINAE context for type existence validation
+
+    Checks if a type is defined in a way this piece of informations can be
+    used in SFINAE contexts.
+
+    @tparam T Type to check existence of.
+    @tparam R Type to return if @c T is defined.
+  **/
+  template<typename T, typename R=void> struct enable_if_type { using type =  R; };
+} } }
+
+#endif
