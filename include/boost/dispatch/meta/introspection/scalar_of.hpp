b4fde99782927586b1b483fb72dd2a78edbb99c9
Charly Chevalier <charly.chevalier@numscale.com>
2015-07-10 23:01:26 +0200

    scalar_of metafuction

---
+++ include/boost/dispatch/meta/introspection/scalar_of.hpp
@@ -0,0 +1,34 @@
+//==================================================================================================
+/*!
+  @file
+
+  Defines the meta::scalar_of meta-function
+
+  @copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_META_INTROSPECTION_SCALAR__OF_HPP_INCLUDED
+#define BOOST_DISPATCH_META_INTROSPECTION_SCALAR__OF_HPP_INCLUDED
+
+#include <boost/dispatch/meta/introspection/detail/scalar_of.hpp>
+
+namespace boost { namespace dispatch
+{
+  /*!
+    @ingroup group-meta
+    @brief Compute the basic value type of an arbitrary type
+
+    Retrieves the most embedded fundamental type of any given type, carrying over
+    cv-qualifiers.
+
+    @tparam T Type to analyze
+  **/
+  template<typename T>
+  using scalar_of = typename ext::scalar_of<T>::type;
+} }
+
+#endif
