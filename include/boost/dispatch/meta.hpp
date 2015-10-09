7940e03ff0d260d59c76f286d1d44a3f39cbb199
Charly Chevalier <charly.chevalier@numscale.com>
2015-10-09 10:26:04 +0200

    More convenience headers

---
+++ include/boost/dispatch/meta.hpp
@@ -0,0 +1,21 @@
+//==================================================================================================
+/*!
+  @file
+
+  Gateway header to hierarchy types
+
+  @copyright 2015 NumScale SAS
+
+  Distributed under the Boost Software License, Version 1.0.
+  (See accompanying file LICENSE.md or copy at http://boost.org/LICENSE_1_0.txt)
+
+**/
+//==================================================================================================
+#ifndef BOOST_DISPATCH_HIERARCHY_HPP_INCLUDED
+#define BOOST_DISPATCH_HIERARCHY_HPP_INCLUDED
+
+#include <boost/dispatch/meta/generation.hpp>
+#include <boost/dispatch/meta/introspection.hpp>
+#include <boost/dispatch/meta/enable_if_type.hpp>
+
+#endif
