8fbee6f50bb452bd123ccbdd27e8d409ce34ebea
Charly Chevalier <charly.chevalier@numscale.com>
2015-10-12 19:51:40 +0200

    Shave off fat from travis script

--- script/travis/install_boost.sh
+++ script/travis/install_boost.sh
@@ -2,7 +2,6 @@
 set -e
 
 # Get Boost 1.58
-wget http://sourceforge.net/projects/boost/files/boost/1.58.0/boost_1_58_0.tar.bz2/download -O /tmp/boost.tar.bz2
+wget http://sourceforge.net/projects/boost/files/boost/1.59.0/boost_1_59_0.tar.bz2/download -O /tmp/boost.tar.bz2
 tar jxf /tmp/boost.tar.bz2
-mv boost_1_58_0/ $HOME/boost-trunk/
-cd $HOME/boost-trunk && ./bootstrap.sh
+mv boost_1_59_0/ $HOME/boost-trunk/
